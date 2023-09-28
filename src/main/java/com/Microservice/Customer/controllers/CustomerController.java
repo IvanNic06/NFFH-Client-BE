551e88e9954299d2b5de02c0e66b3847333889ec
giovdellap <giovdellap@gmail.com>
2023-09-28 17:38:16 +0200

    FUNZIONA

---
+++ src/main/java/com/Microservice/Customer/controllers/CustomerController.java
@@ -0,0 +1,56 @@
+package com.Microservice.Customer.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import com.Microservice.Customer.entities.Customer;
+import com.Microservice.Customer.requests.CreateCustomerInput;
+import com.Microservice.Customer.services.CustomerService;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+public class CustomerController {
+
+    @Autowired
+    public final CustomerService taskService;
+
+    public CustomerController(CustomerService taskService) {
+        this.taskService = taskService;
+    }
+
+    @PostMapping("/customer")
+    public ResponseEntity<Customer> createTask(@RequestBody CreateCustomerInput createTaskInput) {
+        Customer taskCreated = taskService.create(createTaskInput.toTask());
+
+        return new ResponseEntity<>(taskCreated, HttpStatus.CREATED);
+    }
+
+    @GetMapping("/customer")
+    public ResponseEntity<List<Customer>> allTasks() {
+        List<Customer> tasks = taskService.findAll();
+
+        return new ResponseEntity<>(tasks, HttpStatus.OK);
+    }
+
+    @GetMapping("/customer/{id}")
+    public ResponseEntity<Customer> oneTask(@PathVariable int id) {
+        Optional<Customer> optionalTask = taskService.findById(id);
+
+        if (optionalTask.isPresent()) {
+            return new ResponseEntity<>(optionalTask.get(), HttpStatus.OK);
+        }
+
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+
+    @DeleteMapping("/customer/{id}")
+    public ResponseEntity<Void> deleteTask(@PathVariable int id) {
+        taskService.delete(id);
+
+        return ResponseEntity.noContent().build();
+    }
+}
---
+++ src/main/java/com/Microservice/Customer/entities/Customer.java
@@ -0,0 +1,58 @@
+package com.Microservice.Customer.entities;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Table(name = "customers")
+@Entity
+public class Customer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(nullable = false)
+    private Integer id;
+
+    @Column(name="email",unique = true, length = 200, nullable = false)
+    private String Email;
+    
+    @Column(name = "password")
+    private String Password;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getEmail() {
+        return Email;
+    }
+
+    public String getPassword() {
+        return Password;
+    }
+
+    public Customer setId(Integer id) {
+        this.id = id;
+        return this;
+    }
+
+    public Customer setEmail(String name) {
+        this.Email = name;
+        return this;
+    }
+
+    public Customer setPassword(String description) {
+        this.Password = description;
+        return this;
+    }
+
+    @Override
+    public String toString() {
+        return "Task{" +
+                "id=" + id +
+                ", email='" + Email + '\'' +
+                ", Password='" + Password + '\'' +
+                '}';
+    }
+}
