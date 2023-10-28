da8fc700505927ea78f9fd4beaa7c749ed3a5ff3
alessandropisent <alessandro.pisent@hotmail.com>
2023-10-28 23:11:02 +0200

    cdjskncsd

--- src/main/java/com/Microservice/Customer/model/LoginResponse.java
+++ src/main/java/com/Microservice/Customer/model/LoginResponse.java
@@ -4,12 +4,15 @@ public class LoginResponse {
     
     private boolean success;
     private String password;
-    
+    private String id;
+    private String username;
 
     
-    public LoginResponse(boolean success, String password) {
+    public LoginResponse(boolean success, String password, String id, String username) {
         this.success = success;
         this.password = password;
+        this.id = id;
+        this.username = username;
     }
     public boolean isSuccess() {
         return success;
@@ -17,16 +20,26 @@ public class LoginResponse {
     public void setSuccess(boolean success) {
         this.success = success;
     }
-    public String isPassword() {
+    public String getPassword() {
         return password;
     }
     public void setPassword(String password) {
         this.password = password;
     }
-    @Override
-    public String toString() {
-        return "LoginResponse [success=" + success + ", password=" + password + "]";
+    public String getId() {
+        return id;
     }
+    public void setId(String id) {
+        this.id = id;
+    }
+    public String getUsername() {
+        return username;
+    }
+    public void setUsername(String username) {
+        this.username = username;
+    }
+    
+
     
         
     
